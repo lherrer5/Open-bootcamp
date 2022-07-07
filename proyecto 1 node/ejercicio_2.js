1eceaa37214af085045672e489ea3b34c82d757c
lherrer5 <102539568+lherrer5@users.noreply.github.com>
2022-07-07 10:21:57 -0500

    ejercicio2

---
+++ proyecto 1 node/ejercicio_2.js
@@ -0,0 +1,8 @@
+const lista_ejericio2= ["Lina Herrera", 36, false, new Date("october 1 1985"), {
+    titulo: "Un curso de milagros",
+    autor: "Helen Dora Cohn",
+    fecha: 1970,
+    url: 'https://cdn.website-editor.net/639d92120e4f461b8dfd2b1cf3ee82c6/files/uploaded/UCDM%2520completo.pdf'
+}]
+
+console.log(lista_ejericio2)
